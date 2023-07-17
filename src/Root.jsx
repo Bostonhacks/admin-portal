9a0e872a2f2997d41588306106e0de1f07061182
DanYu12 <dan.daniel.yu@gmail.com>
2023-07-17 15:21:32 -0400

    updated auth

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,52 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
+import React from 'react';
+import { auth, logout } from './firebase/firebase-config';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Login from './pages/Login';
 
 const Root = () => {
+  const [user, loading] = useAuthState(auth);
+
+  const valid_emails = [
+    'dfc@bu.edu',
+    'dxu0117@bu.edu',
+    'dlaboy@bu.edu',
+    'declanyg@bu.edu',
+    'simran27@bu.edu',
+    'danyu@bu.edu',
+    'minpark@bu.edu',
+    'jamesw03@bu.edu',
+    'kbbtan@bu.edu',
+    'rongc@bu.edu',
+    'eschoi@bu.edu',
+    'jaketlee@bu.edu',
+    'yangi@bu.edu',
+  ];
+
   return (
     <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
+      {!user ? (
+        <Login />
+      ) : valid_emails.includes(user.email) ? (
+        <>
+          <Navbar />
+          <main>
+            <Outlet />
+          </main>
+        </>
+      ) : (
+        <div>
+          <h1>INVALID USER</h1>
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            LOGOUT
+          </button>
+        </div>
+      )}
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,14 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { auth, signInWithGoogle } from "../firebase/firebase-config";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React from 'react';
+import { signInWithGoogle } from '../firebase/firebase-config';
 import GoogleIcon from '../components/common/GoogleIcon.svg';
 
 // Login page
 export default function Login() {
-  const [user, loading] = useAuthState(auth);
-  const navigate = useNavigate();
-
-  // Add or edit this to all valid emails
-  const valid_emails = ["jamesw03@bu.edu"];
-  
-  useEffect(() => {
-    if (loading) return;
-    if (user) {
-      // Check if the user is a valid admin
-      if (valid_emails.includes(user.email)) {
-        navigate("/stats");
-      } else {
-        // Redirect them to the invalid page and signs out of the authentication stage to re-Authenticate
-        auth.signOut();
-        navigate("/invalid");
-      }
-    }
-
-  }, [user, loading, navigate]);
-
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{ textAlign: 'center' }}>
       <button onClick={signInWithGoogle}>
-        <img src={GoogleIcon} alt="Google Icon" style={{ width: "100px" }}/>Login with Google
+        <img src={GoogleIcon} alt="Google Icon" style={{ width: '100px' }} />
+        Login with Google
       </button>
     </div>
   );
