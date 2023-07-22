c7ec9a0ad40a8bbc6dc51a44e535322916f45582
declanyg <declan@youngs.asia>
2023-07-21 23:55:14 -1000

    Added data read name, status, id

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,46 +1,12 @@
 import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
-/**
- * You might find it useful to have some dummy data for your own testing.
- * Feel free to write this function if you find that feature desirable.
- * 
- * When you come to office hours for help, we will ask you if you have written
- * this function and tested your project using it.
- */
-// export function dummyData() {
-//   return [];
-// }
-
-/**
- * This is the component where you should write the code for displaying the
- * the table of grades.
- *
- * You might need to change the signature of this function.
- *
- */
-
-
-const rows = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
-
-const columns = [
-  { field: 'col1', headerName: 'ID', flex: 1, headerAlign: 'center' },
-  { field: 'col2', headerName: 'Name', flex: 1, headerAlign: 'center' },
-  { field: 'col3', headerName: 'Status', flex: 1, headerAlign: 'center' },
-  { field: 'col4', headerName: 'Actions', flex: 1, headerAlign: 'center' }
-];
-
-const Table = () => {
-
-  return (
-    <div style={{ height: 300, width: '100%' }} className="justify-self-center">
-      <DataGrid rows={rows} columns={columns} />
-    </div>
-  );
+const Table = ({rows, columns}) => {
+    return (
+      <div style={{ height: '80%', width: '100%' }} className="justify-self-center">
+        <DataGrid rows={rows} columns={columns} />
+      </div>
+    );
 }
 
 export default Table;
--- src/pages/ApplicationsPage.jsx
+++ src/pages/ApplicationsPage.jsx
@@ -1,8 +1,50 @@
 import Table from "../components/Table";
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase/firebase-config";
+
+const columns = [
+  { field: 'col1', headerName: 'ID', flex: 1, headerAlign: 'left' },
+  { field: 'col2', headerName: 'Name', flex: 1, headerAlign: 'left' },
+  { field: 'col3', headerName: 'Status', flex: 1, headerAlign: 'left' },
+  { field: 'col4', headerName: 'Actions', flex: 1, headerAlign: 'left' }
+];
 
 const ApplicationsPage = () => {
+
+  const [rowData, setRowData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    
+    const ref = collection(db, "test"); //Temporary database with similar structure. Switch to applications in the final version
+    const querySnapshot = await getDocs(ref);
+    const arr = [];
+    var counter = 0;
+    querySnapshot.forEach((doc) => {
+      arr.push({
+        id: counter, col1: doc.id, col2: doc.get('name'), col3: doc.get('status')
+      });
+      counter += 1;
+    });
+    console.log(arr);
+    setRowData(arr);
+    setLoading(false);
+  };
+   
+    useEffect(()=>{
+      fetchData();
+    }, [])
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
   return (
-  <div> <Table /> </div>
+  <div> <Table rows={rowData} columns ={columns}/> </div>
   );
 };
 
