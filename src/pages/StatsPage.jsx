bf7a9f52831934de7f18568c5b7a0933fb052f1a
dan1elchen <dfc@bu.edu>
2023-07-31 18:33:43 -0400

    Installed chartjs & added bar chart for 2022 college years

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,5 +1,43 @@
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
 const StatsPage = () => {
-  return <div>StatsPage</div>;
+  const data = {
+    labels: ['Not Listed', 'Freshman', 'Sophomore', 'Junior', 'Senior'],
+    datasets: [
+      {
+        label: 'College Year',
+        data: [86, 134, 234, 187, 155],
+        backgroundColor: 'aqua',
+        borderColor: 'black',
+        borderWidth: '1',
+      }
+    ]
+  };
+
+  const options = {
+
+  }
+
+  return (
+  <div>
+    <div
+      style = {
+        {
+          padding: '20px',
+          width: '80%',
+        }
+      }>
+      <Bar
+       data = {data} 
+       options = {options}>
+        
+       </Bar>
+    </div>
+  </div>
+  );
 };
 
 export default StatsPage;
