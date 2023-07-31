7d6299b2ec2b78bca2eec24844da499fcd48db4f
declanyg <declan@youngs.asia>
2023-07-31 01:33:42 -1000

    Show grid toolbar

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
 const Table = ({rows, columns}) => {
     return (
       <div style={{ height: '80%', width: '100%' }} className="justify-self-center">
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid rows={rows} columns={columns} slots={{ toolbar: GridToolbar }} />
       </div>
     );
 }
